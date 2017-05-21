4e5ea531ff2c0e7e44d4d7d8970c4337f5a7ac36
Amber Murray <amber.applebee@gmail.com>
2017-05-21 12:13:30 -0700

    set up table scheme for User

--- app.py
+++ app.py
@@ -10,7 +10,16 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, username, email)
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/')
+def index():
+    return '<h1 style="color: blue">Hello Flask</h1>'
 
 @app.route('/')
 def index():
