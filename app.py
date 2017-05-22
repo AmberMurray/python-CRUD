40c8d8b1b7b98aa8c67b473deb63aea844b919dd
Amber Murray <amber.applebee@gmail.com>
2017-05-21 21:40:51 -0700

    working on update and delete routes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask_movie'
 app.debug = True
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
@@ -19,15 +20,21 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+app.add_url_rule('/users/<id>', methods=['GET', 'PUT', 'DELETE'])
+
 @app.route('/')
 def index():
     myUser = User.query.all()
     oneItem = User.query.filter_by(username="cat").first()
     return render_template('index.html', myUser=myUser, oneItem=oneItem)
 
-@app.route('/profile/<username>')
-def profile(username):
-    user = User.query.filter_by(username=username).first()
+@app.route('/add')
+def add():
+    return render_template('add_user.html')
+
+@app.route('/profile/<id>')
+def profile(id):
+    user = User.query.filter_by(id=id).first()
     return render_template('profile.html', user=user)
 
 @app.route('/post_user', methods=['POST'])
@@ -37,6 +44,18 @@ def post_user():
     db.session.commit()
     return redirect(url_for('index'))
 
+@app.route('/update/<id>', methods=['POST'])
+def update(id):
+    user = User.query.filter_by(id=id).first()
+    db.session.commit()
+    return redirect(url_for('profile'))
+
+@app.route('/delete/<id>', methods=['POST'])
+def delete(id):
+    user = User.query.filter_by(id=id).first()
+    db.session.delete()
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
