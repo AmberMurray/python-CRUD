cd5a8545eed9a0a7a6e873088fbccbdc985161e7
Amber Murray <amber.applebee@gmail.com>
2017-05-21 21:59:09 -0700

    delete route works :sparkles:

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, redirect, url_for, render_template
+from flask import jsonify
+from flask import Response
+import json
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask_movie'
@@ -9,6 +13,7 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
@@ -20,13 +25,10 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-app.add_url_rule('/users/<id>', methods=['GET', 'PUT', 'DELETE'])
-
 @app.route('/')
 def index():
     myUser = User.query.all()
-    oneItem = User.query.filter_by(username="cat").first()
-    return render_template('index.html', myUser=myUser, oneItem=oneItem)
+    return render_template('index.html', myUser=myUser)
 
 @app.route('/add')
 def add():
@@ -44,16 +46,15 @@ def post_user():
     db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/update/<id>', methods=['POST'])
+@app.route('/update/<id>', methods=['GET'])
 def update(id):
     user = User.query.filter_by(id=id).first()
-    db.session.commit()
-    return redirect(url_for('profile'))
+    return render_template('update.html', user=user)
 
 @app.route('/delete/<id>', methods=['POST'])
 def delete(id):
-    user = User.query.filter_by(id=id).first()
-    db.session.delete()
+    user = User.query.filter_by(id=id).first_or_404()
+    db.session.delete(user)
     db.session.commit()
     return redirect(url_for('index'))
 
