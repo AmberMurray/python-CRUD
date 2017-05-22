93046ef9d963b58a03dbcee71674740ee44b1414
Amber Murray <amber.applebee@gmail.com>
2017-05-22 09:31:05 -0700

    fixed db connection

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask_movie'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flask_movie'
 app.debug = True
 db = SQLAlchemy(app)
 
